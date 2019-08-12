7b08a275d54d3ad30a0809e560ecadd76db0aad7
何泽鹏 <275688470@qq.com>
2019-08-12 12:56:16 +0800

    启动时动态识别内网ip，避免内网ip变化时需要重新配置env变量

--- vue.config.js
+++ vue.config.js
@@ -5,6 +5,30 @@ const resolve = dir => path.join(__dirname, dir)
 const name = defaultSettings.title || 'vue-admin' // page title
 const port = 8080 // dev port
 
+const os = require('os')
+
+function getNetworkIp() {
+  let needHost = '' // 打开的host
+  try {
+    // 获得网络接口列表
+    const network = os.networkInterfaces()
+    for (const dev in network) {
+      const iface = network[dev]
+      for (let i = 0; i < iface.length; i++) {
+        const alias = iface[i]
+        if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal) {
+          needHost = alias.address
+        }
+      }
+    }
+  } catch (e) {
+    needHost = 'localhost'
+  }
+  return needHost
+}
+
+process.env.VUE_APP_BASE_API = 'http://' + getNetworkIp() + process.env.VUE_APP_BASE_API
+console.log(process.env.VUE_APP_BASE_API)
 module.exports = {
   publicPath: '/',
   outputDir: 'dist',
